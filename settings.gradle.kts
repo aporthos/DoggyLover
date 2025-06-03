2e2203a3267051ed623025863ff9c5f846f69428
Amadeus Portes <portes@Amadeuss-MacBook-Air.local>
2025-06-02 21:32:21 -0600

    First commit, create skeleton project

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DoggyLover"
+include(":app")
+include(":feature:favorites")
+include(":feature:dogs")
+include(":core:common")
+include(":core:data")
+include(":core:database")
+include(":core:domain")
+include(":core:network")
+include(":core:models:entity")
+include(":core:models:network")
+include(":core:models:domain")
+include(":core:designsystem")
